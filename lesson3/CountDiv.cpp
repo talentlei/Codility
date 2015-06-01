3bb498525a3ddfaad5e615acceffaf3fdccef4a9
talentlei <chenlei_0630@163.com>
2015-06-01 19:38:51 +0800

    CountDiv
    CountDiv

---
+++ lesson3/CountDiv.cpp
@@ -0,0 +1,10 @@
+// you can use includes, for example:
+// #include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int solution(int A, int B, int K) {
+    // write your code in C++11
+        return B/K-A/K+((A%K==0)?1:0);
+}
