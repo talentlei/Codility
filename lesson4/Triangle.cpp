e9bc4fca2b6473f1cb30d449941a1eb6b2b07916
talentlei <chenlei_0630@163.com>
2015-06-02 21:27:11 +0800

    Update Triangle.cpp

--- lesson4/Triangle.cpp
+++ lesson4/Triangle.cpp
@@ -1,11 +1,13 @@
+#include <algorithm>
+#include <vector>
+#include <map>
 int solution(vector<int> &A) {
     // write your code in C++11
-    long sum,big;
+    if (A.size()<3)
+        return 0;
     sort(A.begin(),A.end());
     for(int i=0; i< A.size()-2&& i<A.size(); i++){
-        sum = A[i]+A[i+1];
-        big = A[i+2];
-        if(sum > big)
+        if(A[i]>0 && A[i]>A[i+2]-A[i+1])
             return 1;
     }
     return 0;
